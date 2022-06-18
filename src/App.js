2423035fa335bf8973ce040381b4faf5730ab66d
axit <akshitkumar.321@gmail.com>
2022-06-19 05:13:34 +0530

    release note: loading component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { useState,useEffect, useCallback } from "react";
 import PageLayout from "./container/layout";
+import { Skeleton } from 'antd';
 import './App.css';
+import { Content } from "antd/lib/layout/layout";
 
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000)
+  }, [])
+
   return (
-    <div>
-      <PageLayout/>
-    </div>
+    <>{loading === false ? (
+      <PageLayout/>):(
+        <Content style={{ margin: '24px 16px 0' }}>
+          <Skeleton active />
+        </Content>
+      )}
+    </>
   );
 }
 
